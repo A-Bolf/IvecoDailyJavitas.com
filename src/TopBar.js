48ade1a2e5223101deec343bae3d26f84afb59ed
Bolf Áron <hosc19@gmail.com>
2022-11-01 14:08:49 +0100

    fix navigation bug on mobile

--- src/TopBar.js
+++ src/TopBar.js
@@ -31,7 +31,10 @@ const ResponsiveAppBar = () => {
 
   const handleNavItemClick = (e) => {
     handleCloseNavMenu();
-    navigate(`/${e.target.dataset.route}`);
+    //ternary operator is needed to handle navigation on mobile
+    e.target.dataset.route
+      ? navigate(`/${e.target.dataset.route}`)
+      : navigate(`/${e.target.parentElement.dataset.route}`);
   };
 
   return (
@@ -63,7 +66,7 @@ const ResponsiveAppBar = () => {
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
-              aria-label="account of current user"
+              aria-label="menu drawer"
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}

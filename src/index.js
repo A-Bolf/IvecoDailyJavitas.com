f9b98fa80ca9336db5654b2d1c75aa42bb7cdec8
A-Bolf <hosc19@gmail.com>
2022-11-20 23:28:24 +0100

    add vercel analytics

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Berles from "./Berles";
 import Kapcsolat from "./Kapcsolat";
 import NotFound from "./NotFound";
 import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
 import Layout from "./Layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -28,4 +29,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(sendToVercelAnalytics);
